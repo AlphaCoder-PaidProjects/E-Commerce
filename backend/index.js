bd2b2c25ee229d09df0bf068816660b0d85e814d
SniperNikhil <nikhilmahendrakar02@gmail.com>
2025-03-10 21:09:05 +0530

    Completely Ready with the Project 9 hours 9 minutes

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ const jwt=require("jsonwebtoken")
 const multer=require("multer")
 const path=require("path")
 const cors=require("cors");
-const { type } = require("os");
-const { log } = require("console");
 
 app.use(express.json());
 app.use(cors());
@@ -121,6 +119,157 @@ app.get('/allproducts',async (req,res)=>{
     let products=await Product.find({})
     console.log("All Products Fetched")
     res.send(products);
+})
+
+// Schema creating for USer model
+
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+// Creating EndPints for registering the USer
+
+app.post('/signup',async(req,res)=>{
+    let check=await Users.findOne({email:req.body.email})
+
+    if(check){
+        return res.status(400).json({
+            success:false,
+            error:"Existing User found with same email id "
+        })
+    }
+    let cart={};
+
+    for(let i=0;i<300;i++){
+        cart[i]=0;
+    }
+    const user=new Users({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token=jwt.sign(data,'secret_ecom');
+    res.json({
+        success:true,
+        token
+    })
+})
+
+// Creating EndPoint for user login
+
+app.post('/login',async(req,res)=>{
+    let user = await Users.findOne({
+        email:req.body.email
+    })
+
+    if(user){
+        const passComapare=req.body.password === user.password;
+        if(passComapare){
+            const data={
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+        
+            res.json({
+                success:true,
+                token
+            })
+        }
+        else{
+            res.json({success:false,error:"Invalid Password"})
+        }
+    }else{
+        res.json({success:false,error:"User Not Found"})
+    }
+})
+
+//  creating endpoint for newcollection data
+
+app.get('/newcollections',async(req,res)=>{
+    let products=await Product.find();
+    let newcollection=products.slice(1).slice(-8);
+    console.log("NewCollection Fetched")
+    res.send(newcollection);
+})
+
+// creating endpoint for popular in women section
+app.get('/popularinwomen',async(req,res)=>{
+    let products=await Product.find({category:"women"})
+    let popular_in_women=products.slice(0,4);
+    console.log("Popular in in women fetched")
+    res.send(popular_in_women);
+})
+
+// creating middleware to fetch user
+const fetchUser=async(req,res,next)=>{
+    const token=req.header('auth-token');
+    if(!token){
+        res.status(401).send({errors:"Please authenticate using a valid token"})
+    }
+    else{
+        try {
+            const data=jwt.verify(token,'secret_ecom');
+            req.user=data.user;
+            next();
+        } catch (error) {
+            res.status(401).send({errors:"please authenticate using a valid token"})
+        }
+    }
+}
+
+// creating enpoints for adding products in cartdata
+app.post('/addtocart',fetchUser,async(req,res)=>{
+    console.log("Added",req.body.itemId) 
+    let userData=await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId]+=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Added")
+})
+
+// creating endpoints to remove product from cartdata
+app.post('/removefromcart',fetchUser,async(req,res)=>{
+    console.log("removed",req.body.itemId)
+    let userData=await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0){
+        userData.cartData[req.body.itemId]-=1;
+    }
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Removed")
+})
+
+// creating endpoint to get cartdata
+app.post('/getcart',fetchUser,async(req,res)=>{
+    console.log("Get Cart")
+    let userData=await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
 })
 
 app.listen(port,(error)=>{
--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './NewCollections.css'
-import new_collection from '../Assets/new_collections'
+// import new_collection from '../Assets/new_collections'
 import Item from '../Item/Item'
 
 function NewCollections() {
-  return (
-    <div className='new-collections'>
-        <h1>New Collections</h1>
-        <hr />
-        <div className="collections">
-            {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            })}
+
+    const [new_collection, setNew_Collection] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/newcollections')
+        .then((res)=>res.json())
+        .then((data)=>{
+            setNew_Collection(data)
+        });
+    },[])
+    
+    return (
+        <div className='new-collections'>
+            <h1>New Collections</h1>
+            <hr />
+            <div className="collections">
+                {new_collection.map((item, i) => {
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                })}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default NewCollections
